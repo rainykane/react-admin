b305920fa3f573443b422779de154a8ff5567972
陈浩银 <chen.haoyin@angsivip.com>
2020-04-27 14:17:21 +0800

    feat：新增默认处理图片

---
+++ src/utils/default_images.ts
@@ -0,0 +1,8 @@
+export const Img403 =
+  "https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*j5LCQabCiz8AAAAAAAAAAABkARQnAQ";
+
+export const Img404 =
+  "https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*tVUkTr052wUAAAAAAAAAAABkARQnAQ";
+
+export const Img500 =
+  "https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*PRRMRY9cMPIAAAAAAAAAAABkARQnAQ";
