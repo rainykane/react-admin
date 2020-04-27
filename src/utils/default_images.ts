51d06b83ee69397686f4964867c22cfcabcd1b71
陈浩银 <chen.haoyin@angsivip.com>
2020-04-27 14:19:56 +0800

    feat：添加图片说明

--- src/utils/default_images.ts
+++ src/utils/default_images.ts
@@ -1,8 +1,8 @@
-export const Img403 =
+export const Img403 /** 无权访问 */ =
   "https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*j5LCQabCiz8AAAAAAAAAAABkARQnAQ";
 
-export const Img404 =
+export const Img404 /** 页面丢失/不存在 */ =
   "https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*tVUkTr052wUAAAAAAAAAAABkARQnAQ";
 
-export const Img500 =
+export const Img500 /** 服务器错误 */ =
   "https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*PRRMRY9cMPIAAAAAAAAAAABkARQnAQ";
