9805484421824fe7f6bc8af79c424be877b87865
陈浩银 <chen.haoyin@angsivip.com>
2020-04-27 14:07:19 +0800

    feat：首页展示

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,8 @@ export default [
   {
     path: "/",
     title: "首页",
-    redirect: "/index",
+    name: "首页", // 如果添加了name属性，会在路由表出现对应的路由item
+    // redirect: "/index",
     exact: true, // 表示是否严格匹配，即 location 是否和 path 完全对应上
     component: "@/pages/Index"
     // routes 配置子路由，通常在需要为多个路径增加 layout 组件时使用
