691b87f0a5f39d909c375ef6b75d98012d1718e5
陈浩银 <chen.haoyin@angsivip.com>
2020-04-27 14:25:49 +0800

    feat：新增404页面图片展示

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { PageHeaderWrapper } from "@ant-design/pro-layout";
 
+import { Img404 } from "@/utils/default_images";
+
 export default () => (
   <PageHeaderWrapper title="页面丢失" content="请确认页面是否存在">
-    <div>页面丢失</div>
+    <div style={{ height: "100%" }} className="flex-center">
+      <img src={Img404} alt="404" />
+    </div>
   </PageHeaderWrapper>
 );
