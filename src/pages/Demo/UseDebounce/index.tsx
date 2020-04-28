2bb8060124c395c510e69150ae58e742bc49db65
陈浩银 <chen.haoyin@angsivip.com>
2020-04-28 09:42:54 +0800

    feat：添加输入框防抖demo

--- src/pages/Demo/UseDebounce/index.tsx
+++ src/pages/Demo/UseDebounce/index.tsx
@@ -1,13 +1,13 @@
-import React, { useState, PureComponent } from 'react';
-import { debounce } from 'lodash';
+import React, { useState, PureComponent } from "react";
+import { debounce } from "lodash";
 
-import { Button } from 'antd';
+import { Button } from "antd";
 
 class UseDebounceClass extends PureComponent<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
-      count: 0,
+      count: 0
     };
     /** 也可以这样使用 */
     // this.onClick = debounce(this.onClick, 350);
@@ -16,7 +16,7 @@ class UseDebounceClass extends PureComponent<any, any> {
   onClick = () => {
     const num = this.state.count + 1;
     this.setState({
-      count: num,
+      count: num
     });
   };
 
@@ -37,13 +37,27 @@ export default () => {
     const num = count + 1;
     setCount(num);
   };
+
+  const change = debounce((val: string) => {
+    console.log(val);
+  }, 500);
+
   return (
     <div>
       <Button type="primary" onClick={debounce(() => onClick(), 350)}>
         函数式使用：{count}
       </Button>
-      {'-----------------'}
+      {"-----------------"}
       <UseDebounceClass />
+      {"-----------------"}
+      输入框防抖：
+      <input
+        type="text"
+        className="test"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          change(e.target.value)
+        }
+      />
     </div>
   );
 };
