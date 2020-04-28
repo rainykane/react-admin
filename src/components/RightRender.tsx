fca5f6f35cf131f5589255b7ed151ef56527d91f
陈浩银 <chen.haoyin@angsivip.com>
2020-04-28 15:49:37 +0800

    Delete RightRender.tsx

--- src/components/RightRender.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import { HeartTwoTone, SmileTwoTone } from '@ant-design/icons';
-// import { Card, Typography, Alert } from 'antd';
-// import { PageHeaderWrapper } from '@ant-design/pro-layout';
-
-export const RightRender = () => {
-  return <div className="RightRender">RightRender</div>;
-};
